16e6cdfb1cc3e1907e881de622bbded51e4bbf63
DC* <dc@syncr.com.ar>
2013-07-07 21:02:49 -0300

    Added post method

--- src/Asphyxia/Socked/Socked.php
+++ src/Asphyxia/Socked/Socked.php
@@ -14,9 +14,67 @@ namespace Asphyxia\Socked;
  */
 class Socked {
 
-    public static function fetch($url, $file) {
-        $command = 'curl --socks5-hostname 127.0.0.1:9050 -A "Mozilla/5.0 (X11; Linux i686; rv:7.0.1) Gecko/20100101 Firefox/7.0.1" --cookie PHPSESSID=62b471618147bb026fd0af1ead8467c8 ' . $url . ' -o ' . $file;
+	private $proto = '--socks5-hostname';
+	private $proxy = '127.0.0.1:9050';
+	private $ua = 'Mozilla/5.0 (X11; Linux i686; rv:7.0.1) Gecko/20100101 Firefox/7.0.1';
+	private $params = '-s';
+	private $session = 'c95as9fp2t7d67moe20ftdq8u6';
+	private $debug = true;
+
+
+	public function __construct($options) {
+		$this->setProtocol(isset($options['protocol'])    ? $options['protocol']  : $this->proto);
+		$this->setProxy(isset($options['proxy'])          ? $options['proxy']     : $this->proxy);
+		$this->setUserAgent(isset($options['ua'])         ? $options['ua']        : $this->ua);
+		$this->setParams(isset($options['params'])        ? $options['params']    : $this->params);
+		$this->setSession(isset($options['session'])      ? $options['session']   : $this->session);
+		$this->setDebug(isset($options['debug'])          ? $options['debug']     : $this->debug);
+	}
+
+    public static function fetch($url, $file, $options = null) {
+    	$file = ' -o "' . $file . '" ';
+    	$socked = new Socked($options);
+        return $socked->exec($url, $file);
+    }
+
+    public static function post($url, $fields, $options = null) {
+    	$arrFields = array();
+    	foreach ($fields as $field => $value) {
+    		array_push($arrFields, " -F $field=$value ");
+    	}
+    	$fields = join(' ', $arrFields);
+
+    	$socked = new Socked($options);
+        return $socked->exec($url, $fields);
+    }
+
+    private function exec($url, $params) {
+		$command = 'curl '.$this->params.' '.$this->proto.' '.$this->proxy.' -A "'.$this->ua.'" --cookie PHPSESSID='.$this->session.' '.$url .' '.$params;
+		if ($this->debug) echo $command;
         return shell_exec($command);
     }
     
+    public function setProtocol($proto) {
+    	$this->proto = $proto;
+    }
+    
+    public function setProxy($proxy) {
+    	$this->proxy = $proxy;
+    }
+
+    public function setUserAgent($ua) {
+    	$this->ua = $ua;
+    }
+
+    public function setParams($params) {
+    	$this->params = $params;
+    }
+
+    public function setSession($session) {
+    	$this->session = $session;
+    }
+
+    public function setDebug($debug) {
+    	$this->debug = $debug;
+    }
 }
